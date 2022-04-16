f5a0b85d0114be04cd946e0f169c2b18cb8812fa
niladrischaudhury <niladri.schaudhury@gmail.com>
2022-04-16 17:25:57 +0530

    Heroku specific changes

--- application.py
+++ application.py
@@ -159,8 +159,10 @@ def getCourses():
             chrome_options = webdriver.ChromeOptions()
             chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
             chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--window-size=1400,800")
-            driver = Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
                             chrome_options=chrome_options)
             driver.get(iNeuronHome)
             driver.find_element_by_xpath("//li[contains(@id, 'course-dropdown')]//img").click()
